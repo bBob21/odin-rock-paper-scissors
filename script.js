1c54b1f327dd82b6dbe40404580e065849aee1b4
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-02-24 18:13:25 +1100

    Added function for human input choice and score variables

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 // # ask user to picks
 // # compare result
 
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber];
 }
 
+function getHumanChoice(){
+    let humanChoice = prompt("Choose one of the following:\n - rock\n - paper\n - scissors")
+    console.log(humanChoice)
+}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
