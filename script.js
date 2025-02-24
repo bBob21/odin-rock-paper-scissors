aa0eeb3671f1e7bd1bb059d63eb18bf5d3bbbf57
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-02-24 18:35:36 +1100

    Added the basic game logic and functionality

--- script.js
+++ script.js
@@ -12,9 +12,50 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("Choose one of the following:\n - rock\n - paper\n - scissors")
-    console.log(humanChoice)
+    let humanChoice = prompt("Choose one of the following:\n - rock\n - paper\n - scissors");
+    return humanChoice.toLowerCase();
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(computerChoice, humanChoice){
+    // Win lose funcs
+    let humanWin = function (computerChoice, humanChoice) {
+        humanScore++;
+        console.log(`>> You win! ${humanChoice} beats ${computerChoice}`)
+    }
+    let computerWin = function (computerChoice, humanChoice) {
+        computerScore++;
+        console.log(`>> You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+
+    // Game state
+    console.log(`You played ${humanChoice}. Computer played ${computerChoice}.`);
+
+    // Game logic
+    if (computerChoice == humanChoice){
+        console.log(">> It's a draw!");
+    }
+    else if (computerChoice == "rock"){
+        if (humanChoice == "paper")
+            humanWin(computerChoice, humanChoice);
+        else if (humanChoice == "scissors")
+            computerWin(computerChoice, humanChoice);
+    }
+    else if (computerChoice == "paper"){
+        if (humanChoice == "rock")
+            computerWin(computerChoice, humanChoice);
+        else if (humanChoice == "scissors")
+            humanWin(computerChoice, humanChoice);
+    }
+    else if (computerChoice == "scissors"){
+        if (humanChoice == "rock")
+            humanWin(computerChoice, humanChoice);
+        if (humanChoice == "paper")
+            computerWin(computerChoice, humanChoice);
+    }
+
+    console.log(`----- Score -----\nComputer: ${computerScore}\nHuman: ${humanScore}`)
+}
+
+playRound(getComputerChoice(), getHumanChoice());
+
+
