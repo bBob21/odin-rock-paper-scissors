bed2f9712e596ec557e6a20b0ab975d7a231a372
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-02-24 18:51:52 +1100

    Added 5 rounds per game and finishing touches

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 // # ask user to picks
 // # compare result
 
-let humanScore = 0
-let computerScore = 0
-
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random() * choices.length);
@@ -17,45 +14,68 @@ function getHumanChoice(){
 }
 
 function playRound(computerChoice, humanChoice){
-    // Win lose funcs
-    let humanWin = function (computerChoice, humanChoice) {
-        humanScore++;
-        console.log(`>> You win! ${humanChoice} beats ${computerChoice}`)
-    }
-    let computerWin = function (computerChoice, humanChoice) {
-        computerScore++;
-        console.log(`>> You lose! ${computerChoice} beats ${humanChoice}`)
-    }
-
     // Game state
-    console.log(`You played ${humanChoice}. Computer played ${computerChoice}.`);
+    console.log(`  You played ${humanChoice}. Computer played ${computerChoice}.`);
 
     // Game logic
     if (computerChoice == humanChoice){
-        console.log(">> It's a draw!");
+        return "draw";
     }
     else if (computerChoice == "rock"){
         if (humanChoice == "paper")
-            humanWin(computerChoice, humanChoice);
+            return "win";
         else if (humanChoice == "scissors")
-            computerWin(computerChoice, humanChoice);
+            return "lose";
     }
     else if (computerChoice == "paper"){
         if (humanChoice == "rock")
-            computerWin(computerChoice, humanChoice);
+            return "lose";
         else if (humanChoice == "scissors")
-            humanWin(computerChoice, humanChoice);
+            return "win";
     }
     else if (computerChoice == "scissors"){
         if (humanChoice == "rock")
-            humanWin(computerChoice, humanChoice);
+            return "win";
         if (humanChoice == "paper")
-            computerWin(computerChoice, humanChoice);
+            return "lose";
     }
-
-    console.log(`----- Score -----\nComputer: ${computerScore}\nHuman: ${humanScore}`)
 }
 
-playRound(getComputerChoice(), getHumanChoice());
+function playGame(){
+    // Score
+    let humanScore = 0
+    let computerScore = 0
 
+    console.log("=== Welcome to Rock Paper Scissors! Five rounds will be played. Start! ===");
 
+    // Loop for multiple rounds
+    let i = 0
+    while (i < 5){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        // Start round
+        console.log(`--- Round ${i+1} ---`)
+        let roundState = playRound(computerChoice, humanChoice);
+
+        // Win or lose, update scores
+        if (roundState == "win") {
+            humanScore++;
+            console.log(`  >> You win! ${humanChoice} beats ${computerChoice}.`);
+        }
+        else if (roundState == "lose") {
+            computerScore++;
+            console.log(`  >> You lose! ${computerChoice} beats ${humanChoice}.`);
+        }
+        else if (roundState == "draw"){
+            console.log("  >> You drew! No one wins.");
+        }
+
+        console.log(`  Score:  Human: ${humanScore} - Computer: ${computerScore}`);
+        i++;
+    }
+
+    console.log(`Final Score! Human: ${humanScore} - Computer: ${computerScore}`);
+}
+
+playGame();
